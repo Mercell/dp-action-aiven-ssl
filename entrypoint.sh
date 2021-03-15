4edbd7c2948b44e745be7fd7f92e2a15fb3731bf
Regine Urtegard <regine.urtegard@mercell.com>
2021-03-15 14:28:15 +0100

    removed quotations added around the password

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ cert_response=$(aws ssm get-parameter --name ${SERVICE_CERT_PATH} --query "Param
 
 ca_response=$(aws ssm get-parameter --name ${CA_PATH} --query "Parameter.Value")
 
+keystore_password_response=$(aws ssm get-parameter --name ${KEYSTORE_PASSWORD_PATH} --with-decryption --query "Parameter.Value")
+
+truststore_password_response=$(aws ssm get-parameter --name ${TRUSTSTORE_PASSWORD_PATH} --with-decryption --query "Parameter.Value")
+
 echo -e "$key_response" | sed -e 's/^"//' -e 's/"$//' > service.key
 echo "$(cat service.key)"
 
@@ -13,9 +17,9 @@ echo "$(cat service.cert)"
 echo -e "$ca_response" | sed -e 's/^"//' -e 's/"$//' > ca.pem
 echo "$(cat ca.pem)"
 
-KEYSTORE_PASSWORD=$(aws ssm get-parameter --name ${KEYSTORE_PASSWORD_PATH} --with-decryption --query "Parameter.Value")
+KEYSTORE_PASSWORD=$(sed -e 's/^"//' -e 's/"$//' <<< $keystore_password_response)
 
-TRUSTSTORE_PASSWORD=$(aws ssm get-parameter --name ${TRUSTSTORE_PASSWORD_PATH} --with-decryption --query "Parameter.Value")
+TRUSTSTORE_PASSWORD=$(sed -e 's/^"//' -e 's/"$//' <<< $truststore_password_response)
 
 openssl pkcs12 -export -inkey service.key -in service.cert -out client.keystore.p12 -name service_key -passout pass:${KEYSTORE_PASSWORD}
 keytool -import -file ca.pem -alias CA -keystore client.truststore.jks -storepass ${TRUSTSTORE_PASSWORD} -noprompt
