7c04806c9bab745706f6b99da5db0547f6db4245
Regine Urtegard <regine.urtegard@mercell.com>
2021-04-23 12:57:19 +0200

    assume aws credentials is set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
-aws sts assume-role --role-arn ${AWS_ROLE_ARN} --role-session-name dp-action-aiven-ssl
+# aws sts assume-role --role-arn ${AWS_ROLE_ARN} --role-session-name dp-action-aiven-ssl
+
+eval $(aws sts assume-role --role-arn arn:aws:iam::123456789123:role/myAwesomeRole --role-session-name test | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 
 key_response=$(aws ssm get-parameter --name ${SERVICE_KEY_PATH} --with-decryption --query "Parameter.Value")
 
